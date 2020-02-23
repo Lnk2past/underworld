2408f0b28c51e1c96be7203582afff4c2b7b1d0d
Lnk2past <Lnk2past@gmail.com>
2020-02-23 17:11:25 -0500

    Allow hull_percentage to trigger on another ship

--- underworld/event_manager/triggers.py
+++ underworld/event_manager/triggers.py
@@ -1,4 +1,6 @@
-def hull_percentage(threshold):
+def hull_percentage(threshold, other=None):
     def wrapper(s):
+        if other:
+            return other.hull < threshold * other.max_hull
         return s.hull < threshold * s.max_hull
     return wrapper
